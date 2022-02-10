6f3d46b181f0061a1664c7a9bee13983422c66db
Zadamm <adam.borocz@gmail.com>
2022-02-11 00:36:41 +0100

    mod3vid15

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Adam', age: 31},
+      { name: 'Anda', age: 21},
+      { name: 'Kata', age: 41}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hi, I'm a React App</h1>
+        <p>This is working!</p>
+        <button>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work?'))
   }
 }
 
